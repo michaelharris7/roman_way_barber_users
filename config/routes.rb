5c9d762ab9c22ab769dc287aa91d2b014bcd228a
Michael Harris <michaeljh7@gmail.com>
2018-04-17 22:24:58 -0600

    Implemented Devise Token Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+
+  mount_devise_token_auth_for 'Admin', at: 'admin_auth'
+  as :admin do
+    # Define routes for Admin within this block.
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
