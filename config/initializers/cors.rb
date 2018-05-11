9c1b7723106fd5c95977b756dc66567f983600e3
Michael Harris <michaeljh7@gmail.com>
2018-05-11 17:17:06 -0600

    Made more progress, but can only test .cors file in production.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'romanway-barber-frontend.herokuapp.com', 'localhost:4200'
+    origins 'romanway-barber-frontend.herokuapp.com'
 
+    # , 'localhost:4200'
     # 'https://romanway-barber-users.herokuapp.com', 'http://localhost:3000'
 
     resource '*',
