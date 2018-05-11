f040528add9a56194cb93cf8a395eff828239812
Michael Harris <michaeljh7@gmail.com>
2018-05-11 16:13:58 -0600

    Testing with cors.rb again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['API_BASE_DEV'], ENV['API_BASE_PROD'], ENV['API_BASE_FRONT']
+    origins 'http://localhost:3000', 'https://romanway-barber-users.herokuapp.com', 'https://romanway-barber-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
