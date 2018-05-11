639d4c2a142f4dc118094abbeda711a4ffd98b41
Michael Harris <michaeljh7@gmail.com>
2018-05-11 17:06:48 -0600

    Changed content in cors.rb again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://romanway-barber-users.herokuapp.com', 'https://romanway-barber-frontend.herokuapp.com', 'http://localhost:4200', 'http://localhost:3000'
+    origins 'romanway-barber-frontend.herokuapp.com', 'localhost:4200'
+
+    # 'https://romanway-barber-users.herokuapp.com', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
