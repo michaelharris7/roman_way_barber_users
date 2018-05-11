52e1edb532a8b122f6e48d382fa5c780d5b209c3
Michael Harris <michaeljh7@gmail.com>
2018-05-11 16:50:26 -0600

    Got everything working fine. Fixed cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://romanway-barber-users.herokuapp.com', 'https://romanway-barber-frontend.herokuapp.com'
+    origins 'https://romanway-barber-users.herokuapp.com', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
