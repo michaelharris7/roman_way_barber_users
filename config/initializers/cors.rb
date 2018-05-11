df167fc911094efd20a2b625640d560f9e1fdb10
Michael Harris <michaeljh7@gmail.com>
2018-05-11 16:05:46 -0600

    Updated rack cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['API_BASE_DEV'], ENV['API_BASE_PROD'], ENV['API_BASE_FRONT']
+    origins 'http://localhost:3000'
+    # ENV['API_BASE_DEV'], ENV['API_BASE_PROD'], ENV['API_BASE_FRONT']
 
     resource '*',
       headers: :any,
