b0f19960d3041da62297bbe17e45a2b0bcc7b07f
Michael Harris <michaeljh7@gmail.com>
2018-05-12 16:02:12 -0600

    Fixed routes and devise initializer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   mount_devise_token_auth_for 'Admin', at: 'admin_auth'
     # Define routes for Admin within this block.
-  end
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
