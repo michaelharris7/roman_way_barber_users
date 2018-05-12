56f2eb1529b338579a288a1cf6bd2879759da3b5
Michael Harris <michaeljh7@gmail.com>
2018-05-12 15:57:54 -0600

    made changes to devise_auth_token initializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'Admin', at: 'admin_auth'
   mount_devise_token_auth_for 'User', at: 'auth'
-  # as :admin do
+  mount_devise_token_auth_for 'Admin', at: 'admin_auth'
     # Define routes for Admin within this block.
-  # end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
