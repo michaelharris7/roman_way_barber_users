525036e1ca0bcbb73d061495158fe3b5d3c8f441
Michael Harris <michaeljh7@gmail.com>
2018-05-16 22:23:49 -0600

    Updated action mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,11 +12,19 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3003 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => ENV.fetch('ACTION_MAILER_ADDRESS'),
+    :port                 => 587,
+    :user_name            => ENV.fetch('ACTION_MAILER_EMAIL'),
+    :password             => ENV.fetch('ACTION_MAILER_PASSWORD'),
+    :authentication       => :login,
+    :enable_starttls_auto => true  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'romanway-barber-users.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => ENV.fetch('ACTION_MAILER_ADDRESS'),
+    :port                 => 587,
+    :user_name            => ENV.fetch('ACTION_MAILER_EMAIL'),
+    :password             => ENV.fetch('ACTION_MAILER_PASSWORD'),
+    :authentication       => :login,
+    :enable_starttls_auto => true  }
 end
